3154ddb130e67de7de18ccc732848fa9b7897ca6
EDUARDO BRAUN <eduardo@rushers.org>
2015-07-21 02:30:03 -0300

    Removed unecessary service from renderer.

--- enki/renderer/renderer.hpp
+++ enki/renderer/renderer.hpp
@@ -23,9 +23,7 @@ class renderer {
   typedef std::function<void()> work_t;
   renderer()
       : render_service_(),
-        result_service_(),
         render_work_(std::make_unique<asio_work>(render_service_)),
-        result_work_(result_service_),
         running_(false),
         working_(false),
         queue_lock_() {}
@@ -36,33 +34,26 @@ class renderer {
     if (!running_) {
       render_worker = std::thread(std::bind(&self::run_render_service, this));
       render_worker.detach();
-      result_worker = std::thread(std::bind(&self::run_result_service, this));
-      result_worker.detach();
       running_ = true;
     }
   }
 
   void stop() {
     if (running_) {
-      /* render_service_.stop(); */
       render_work_.reset();
       render_worker.join();
-      result_service_.stop();
-      result_worker.join();
+      render_service_.reset();
       running_ = false;
     }
   }
 
   void add_work(int p, work_t work) {
-    /* render_service_.post(work); */
     queue_lock_.lock();
     work_queue_.push(queued_handler(p, work));
     if (working_ == false)
       render_service_.post(std::bind(&self::queue_handler, this));
     queue_lock_.unlock();
   }
-
-  void add_result(work_t result) { result_service_.post(result); }
 
  private:
   class queued_handler {
@@ -98,15 +89,9 @@ class renderer {
 
   void run_render_service() { render_service_.run(); }
 
-  void run_result_service() { result_service_.run(); }
-
   std::thread render_worker;
-  std::thread result_worker;
   boost::asio::io_service render_service_;
-  boost::asio::io_service result_service_;
-  /* boost::asio::io_service::work render_work_; */
   std::unique_ptr<asio_work> render_work_;
-  boost::asio::io_service::work result_work_;
   std::priority_queue<queued_handler> work_queue_;
   std::atomic_bool running_;
   std::atomic_bool working_;
